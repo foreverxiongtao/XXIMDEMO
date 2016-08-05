de979e6695c2e62fc72ec249e6d05d2cdef5beea
quzongyang <quzongyang@xuxian.com>
2016-08-05 17:32:58 +0800

    add

--- MyApplication/app/src/main/java/com/xuxian/xximdemo/ui/MainActivity.java
+++ MyApplication/app/src/main/java/com/xuxian/xximdemo/ui/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn_sign_in_button = (Button) findViewById(R.id.btn_sign_in_button);
         btn_sign_in_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
